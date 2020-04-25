9f74a65d2f755a7152abc6a94a4c1cd421b038d2
ZengLi5336 <1148082633@qq.com>
2020-04-25 04:30:31 +0000

    Add Bing background.

--- app/src/main/java/com/coolweather/android/db/Province.java
+++ app/src/main/java/com/coolweather/android/db/Province.java
@@ -7,12 +7,12 @@ public class Province extends DataSupport {
     private String provinceName;
     private int provinceCode;
 
-    public int getId(){
+    public int getId() {
         return id;
     }
 
-    public void setId(int id){
-        this.id=id;
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getProvinceName() {
